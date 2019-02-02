6529ea6dc8d2a9598da3aa191bcd057cc2e16f0d
giosd <giovanni_0611@hotmail.com>
2019-02-02 13:52:12 -0200

    conection1

---
+++ conect.php
@@ -0,0 +1,21 @@
+<?php
+
+$dbhost   = “locahost”;   #Nome do host
+
+$connectionInfo = array("Database"=>"verao-2019","UID"=>"server1","PWD"=>"123456")
+
+https://www.youtube.com/watch?v=q9CFmbMvXfY
+
+
+@mssql_connect($dbhost,$user,$password) or die
+
+(“Não foi possível a conexão com o servidor!”);
+
+
+@mssql_select_db(“$db“) or die
+
+(“Não foi possível selecionar o banco de dados!”);
+
+
+
+?>
